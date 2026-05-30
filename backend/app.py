58a1ce061019a3f043052326d65e84392a51c880
Xceptions <agbokenechukwu.k@gmail.com>
2026-05-30 13:41:22 +0100

    modified app.py to return data properly

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,8 @@ model = joblib.load("model.joblib")
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    features = np.array(data['features']).reshape(1, -1)
+    features = [float(x) for x in data]
+    features = np.array(features).reshape(1, -1)
     prediction = model.predict(features)
     
     return jsonify({'prediction': int(prediction[0])})
