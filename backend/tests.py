f416e3ef053743c6b24e2773512d80975978af39
Xceptions <agbokenechukwu.k@gmail.com>
2026-05-30 14:38:50 +0100

    fixed issue of docker not finding exact package version in requirements.txt. Also had to update tests to reflect new way of serving api

--- backend/tests.py
+++ backend/tests.py
@@ -5,7 +5,7 @@ def test_predict():
     '''Testing the predict api with random data'''
     url = "http://127.0.0.1:5000/predict"
     headers = {"Content-Type": "application/json"}
-    payload = {"features": [5.1, 3.5, 1.4, 0.2]}
+    payload = [5.1, 3.5, 1.4, 0.2]
 
     response = requests.post(url, json=payload, headers=headers)
     print(response.status_code)
